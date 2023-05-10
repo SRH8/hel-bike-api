ccccf38d00937170433228af8af109e726ed6d88
Sergio Fraga <sergiofraga.cs@gmail.com>
2023-05-10 21:23:43 +0200

    Create class to store application constants

---
+++ src/main/java/com/sergiofraga/helsinkibikeapi/util/AppConstants.java
@@ -0,0 +1,16 @@
+package com.sergiofraga.helsinkibikeapi.util;
+
+/**
+ * This class stores constants used by the application
+ */
+public class AppConstants {
+
+    private  AppConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public  static final String DEFAULT_PAGE_SIZE = "15";
+    public static final String DEFAULT_STATIONS_SORT_BY = "nimi";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
+}
