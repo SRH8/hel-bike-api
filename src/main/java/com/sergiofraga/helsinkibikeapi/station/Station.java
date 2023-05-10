8475f655611aaf0eaf8e7941ae2351eacc47692f
Sergio Fraga <sergiofraga.cs@gmail.com>
2023-05-10 19:03:12 +0200

    Create Station JPA entity

---
+++ src/main/java/com/sergiofraga/helsinkibikeapi/station/Station.java
@@ -0,0 +1,36 @@
+package com.sergiofraga.helsinkibikeapi.station;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "stations")
+public class Station {
+
+    @Id
+    @GeneratedValue
+    private int fid;
+    private int id;
+    private String nimi;
+    private String namn;
+    private String name;
+    private String osoite;
+    private String adress;
+    private String kaupunki;
+    private String stad;
+    private String operaattor;
+    private int kapasiteet;
+    private double x;
+    private double y;
+}
