8a5f33182f0d00dfc8111fcbd71292c9dc2ea9bf
Sergio Fraga <sergiofraga.cs@gmail.com>
2023-05-10 18:25:01 +0200

    First commit

---
+++ src/main/java/com/sergiofraga/helsinkibikeapi/HelsinkiBikeApiApplication.java
@@ -0,0 +1,13 @@
+package com.sergiofraga.helsinkibikeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelsinkiBikeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelsinkiBikeApiApplication.class, args);
+	}
+
+}
